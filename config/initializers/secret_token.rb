f06daf124aaac72b97fdc529b438964229c6944c
Paul Hayes <github@prhayes.com>
2014-02-18 22:02:28 +0000

    Initial commit
    * Default Rails 4 app scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Preserve::Application.config.secret_key_base = '35770024b03f7e5bc0e7df2c8c8a51c23f017ca1b648e36deaf50fa65cea62bb882d49dd9deff87c0e4528461a32ffb00e7cf095ee8c87b4530b8dbe586951f9'
